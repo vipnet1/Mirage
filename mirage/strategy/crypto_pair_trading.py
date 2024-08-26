f7103e10248809c0dfc602f8be87907107c7edc0
vipnet1 <timur.pichkhadze@gmail.com>
2024-08-26 22:23:53 +0300

    add borrow logic pair trading strategy

--- mirage/strategy/crypto_pair_trading.py
+++ mirage/strategy/crypto_pair_trading.py
@@ -1,15 +1,21 @@
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Optional
+import logging
+from typing import Optional, Tuple
 
 from bson import ObjectId
 import pymongo
 import consts
+from mirage.brokers.binance.binance import Binance
 from mirage.config.config_manager import ConfigManager
-from mirage.history.common_operations import insert_dataclass, update_dataclass
+from mirage.history.common_operations import insert_dataclass, insert_dict, update_dataclass
 from mirage.history.history_db_config import HistoryDbConfig
 from mirage.strategy.strategy import Strategy
 from mirage.utils.dict_utils import dataclass_to_dict
+
+
+class CryptoPairTradingException(Exception):
+    pass
 
 
 @dataclass
@@ -42,6 +48,10 @@ class CryptoPairTrading(Strategy):
 
     DATA_PAIR = 'pair'
     DATA_SIDE = 'side'
+    DATA_STOPLOSS_DISTANCE = 'stoploss_distance'
+
+    SIDE_LONG = 'long'
+    SIDE_SHORT = 'short'
 
     description = 'Go long & short on pairs. Binance margin account.'
 
@@ -55,7 +65,7 @@ class CryptoPairTrading(Strategy):
             self._close_current_position(pair_info, existing_position)
             return
 
-        self._enter_new_position(pair_info)
+        await self._enter_new_position(pair_info)
 
     def _get_recent_position_info(self) -> Optional[PositionInfo]:
         recent_position = self._get_recent_position_info_from_db()
@@ -68,19 +78,92 @@ class CryptoPairTrading(Strategy):
 
         return position_info
 
-    def _enter_new_position(self, pair_info: PairInfo):
+    async def _enter_new_position(self, pair_info: PairInfo):
+        coin1_amount, coin2_amount = self._calculate_positions_for_coins(pair_info.ratio)
+        side = self._strategy_data.get(CryptoPairTrading.DATA_SIDE)
+
+        if side == CryptoPairTrading.SIDE_LONG:
+            await self._binance_enter_new_position(pair_info.first_pair, coin1_amount, pair_info.second_pair, coin2_amount)
+        elif side == CryptoPairTrading.SIDE_SHORT:
+            await self._binance_enter_new_position(pair_info.first_pair, coin1_amount, pair_info.second_pair, coin2_amount)
+        else:
+            raise CryptoPairTradingException(f'Invalid side received: {side}')
+
+        pair = pair_info.first_pair.split('/')[0] + '/' + pair_info.second_pair.split('/')[0]
         insert_dataclass(
             consts.DB_NAME_STRATEGY_CRYPTO_PAIR_TRADING,
             consts.COLLECTION_POSITION_INFO,
             PositionInfo(
-                chart_pair='EOS/USDT-6580*WIN/USDT',
-                side='long',
-                pair='EOS/WIN',
+                chart_pair=self._strategy_data.get(CryptoPairTrading.DATA_PAIR),
+                side=side,
+                pair=pair,
                 is_open=True,
-                first_amount=5,
-                second_amount=10
+                first_amount=coin1_amount,
+                second_amount=coin2_amount
             )
         )
+
+    async def _binance_enter_new_position(self, long_pair: str, long_amount: float, short_pair: str, short_amount: float):
+        binance = Binance()
+        async with binance.exchange:
+            logging.info('Borrowing coin on binance')
+            shorted_coin = short_pair.split('/')[0]
+            borrow_order = await binance.exchange.borrow_cross_margin(shorted_coin, short_amount)
+            insert_dict(
+                consts.DB_NAME_HISTORY,
+                consts.COLLECTION_BROKER_RESPONSE,
+                {
+                    'request_data_id': self._request_data_id,
+                    'broker': 'binance',
+                    'description': 'Borrowed coin to short',
+                    'content': borrow_order
+                }
+            )
+
+            # logging.info('Going long on binance')
+            # long_order = await binance.exchange.create_order(
+            #     symbol=long_pair,
+            #     type='market',
+            #     side='buy',
+            #     amount=long_amount,
+            #     positionSide='long',
+            #     leverage=2,  # Replace with your desired leverage
+            #     marginMode='cross'  # Specify cross margin mode
+            # )
+            # long_order = await binance.exchange.create_margin_order(
+            #     symbol=long_pair,
+            #     side='buy',
+            #     type='market',
+            #     quantity=long_amount
+            # )
+            # insert_dict(
+            #     consts.DB_NAME_HISTORY,
+            #     consts.COLLECTION_BROKER_RESPONSE,
+            #     {
+            #         'request_data_id': self._request_data_id,
+            #         'broker': 'binance',
+            #         'description': 'Went long in some pair',
+            #         'content': long_order
+            #     }
+            # )
+
+            # logging.info('Going short on binance')
+            # short_order = await binance.exchange.create_margin_order(
+            #     symbol=short_pair,
+            #     side='sell',
+            #     type='market',
+            #     quantity=short_amount
+            # )
+            # insert_dict(
+            #     consts.DB_NAME_HISTORY,
+            #     consts.COLLECTION_BROKER_RESPONSE,
+            #     {
+            #         'request_data_id': self._request_data_id,
+            #         'broker': 'binance',
+            #         'description': 'Went long in some pair',
+            #         'content': short_order
+            #     }
+            # )
 
     def _close_current_position(self, pair_info: PairInfo, position_info: PositionInfo):
         update_dataclass(
@@ -110,7 +193,12 @@ class CryptoPairTrading(Strategy):
 
         return PairInfo(first_pair, second_pair, ratio)
 
-    def _calculate_positions(self, pair_info: PairInfo):
+    def _calculate_positions_for_coins(self, ratio: float) -> Tuple[float, float]:
         allocated_capital = ConfigManager.config.get(CryptoPairTrading.KEY_ALLOCATED_CAPITAL)
         max_loss_percent = ConfigManager.config.get(CryptoPairTrading.KEY_MAX_LOSS_PERCENT)
-        side = self._strategy_data.get(CryptoPairTrading.DATA_SIDE)
+        stoploss_distance = self._strategy_data.get(CryptoPairTrading.DATA_STOPLOSS_DISTANCE)
+
+        coin1_amount = allocated_capital * (max_loss_percent / 100) / stoploss_distance
+        coin2_amount = coin1_amount * ratio
+
+        return coin1_amount, coin2_amount
--- mirage/strategy/strategy.py
+++ mirage/strategy/strategy.py
@@ -12,7 +12,9 @@ class Strategy:
 
     def __init__(self, strategy_data: Dict[str, Any]):
         self._strategy_data = MirageDict(strategy_data)
+        self._request_data_id = None
 
     @abstractmethod
     async def execute(self, request_data_id: str):
+        self._request_data_id = request_data_id
         logging.info('Executing %s strategy', self.__class__.__name__)

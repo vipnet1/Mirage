d1fe53ae4677ffd2516abba914267ec953bc4d57
Timur Pichkhadze <timur.pichkhadze@gmail.com>
2024-11-19 01:53:27 +0200

    fix self update wrong venv

--- mirage/jobs/self_update/self_update_job.py
+++ mirage/jobs/self_update/self_update_job.py
@@ -8,12 +8,13 @@ from mirage.utils.command_utils import run_command_async
 class SelfUpdateJob(MirageJob):
 
     async def execute(self):
+        await run_command_async('git fetch origin')
         latest_commit = await run_command_async('git rev-parse origin/' + consts.MIRAGE_MAIN_BRANCH)
         current_commit = await run_command_async('git rev-parse HEAD')
 
         if latest_commit != current_commit:
             logging.info('Newer mirage version detected.')
             ConfigManager.execution_config.set(consts.EXECUTION_CONFIG_KEY_UPDATE, True)
-            ConfigManager.execution_config.set(consts.EXECUTION_CONFIG_KEY_TERMINATE, False)
+            ConfigManager.execution_config.set(consts.EXECUTION_CONFIG_KEY_TERMINATE, True)
 
         self._reset_job()

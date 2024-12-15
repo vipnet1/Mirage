c71d176ffdee89f8d899d1aff04f99eec5d2d712
Timur Pichkhadze <timur.pichkhadze@gmail.com>
2024-12-16 01:11:56 +0200

    begin working on improved performance summary

--- mirage/channels/telegram/commands/performance/show_summary.py
+++ mirage/channels/telegram/commands/performance/show_summary.py
@@ -1,77 +1,36 @@
 import csv
 import os
 import tempfile
-import consts
 from mirage.channels.channels_manager import ChannelsManager
+from mirage.channels.telegram.exceptions import MirageTelegramException
 from mirage.channels.telegram.telegram_command import TelegramCommand
-from mirage.database.mongo.common_operations import get_records
-from mirage.performance.mirage_performance import DbTradePerformance
+from mirage.performance.summary_report_generator import SummaryReportGenerator
 
 
 class PerformaceSummaryCommand(TelegramCommand):
-    STRATEGY = 'strategy'
-    INSTANCE = 'instance'
-    RECORDS_COUNT = 'records_count'
-    TOTAL_PROFIT = 'total_profit'
-    TOTAL_CAPITAL = 'total_capital'
-    AVERAGE_PROFIT = 'average_profit'
-    AVERAGE_CAPITAL = 'average_capital'
+    DATE_ALL = 'ALL'
 
     async def execute(self) -> None:
-        records = get_records(consts.DB_NAME_MIRAGE_PERFORMANCE, consts.COLLECTION_TRADE_OUTCOMES, {})
+        date_from = self._get_top_line()
+        if not date_from:
+            raise MirageTelegramException(f'Provide date from. For example 2024-02-15. For all time write {PerformaceSummaryCommand.DATE_ALL}.')
 
-        performance_summary = self._create_totals_summary(records)
+        self._remove_first_line()
 
-        results_for_csv = self._generate_results_for_csv(performance_summary)
+        date_to = self._get_top_line()
+        if not date_to:
+            raise MirageTelegramException(f'Provide date to. For example 2024-02-15. For all time write {PerformaceSummaryCommand.DATE_ALL}.')
+
+        self._remove_first_line()
+
+        if date_from == PerformaceSummaryCommand.DATE_ALL:
+            date_from = ''
+
+        if date_to == PerformaceSummaryCommand.DATE_ALL:
+            date_to = ''
+
+        results_for_csv = await SummaryReportGenerator().generate_report(date_from, date_to)
         await self._send_results_csv(results_for_csv)
-
-    def _create_totals_summary(self, records) -> dict[str, any]:
-        performance_summary = {}
-        for record in records:
-            db_trade_performance = DbTradePerformance(**record)
-            if db_trade_performance.strategy_name not in performance_summary:
-                performance_summary[db_trade_performance.strategy_name] = {}
-
-            strategy_info = performance_summary[db_trade_performance.strategy_name]
-            if db_trade_performance.strategy_instance not in strategy_info:
-                strategy_info[db_trade_performance.strategy_instance] = {}
-
-            instance_info = strategy_info[db_trade_performance.strategy_instance]
-            self._populate_instance_info(instance_info, db_trade_performance)
-
-        return performance_summary
-
-    def _populate_instance_info(self, instance_info: dict[str, any], db_trade_performance: DbTradePerformance):
-        if self.RECORDS_COUNT not in instance_info:
-            instance_info[self.RECORDS_COUNT] = 0
-
-        if self.TOTAL_PROFIT not in instance_info:
-            instance_info[self.TOTAL_PROFIT] = 0
-
-        if self.TOTAL_CAPITAL not in instance_info:
-            instance_info[self.TOTAL_CAPITAL] = 0
-
-        instance_info[self.RECORDS_COUNT] += 1
-        instance_info[self.TOTAL_PROFIT] += db_trade_performance.profit
-        instance_info[self.TOTAL_CAPITAL] += db_trade_performance.available_capital
-
-    def _generate_results_for_csv(self, performance_summary: dict[str, any]) -> list[dict[str, any]]:
-        results = []
-
-        for strategy in performance_summary:
-            strategy_data = performance_summary[strategy]
-            for instance in strategy_data:
-                data = strategy_data[instance]
-                data[self.STRATEGY] = strategy
-                data[self.INSTANCE] = instance
-                self._populate_averages(data)
-                results.append(data)
-
-        return results
-
-    def _populate_averages(self, data: dict[str, any]) -> None:
-        data[self.AVERAGE_PROFIT] = data[self.TOTAL_PROFIT] / data[self.RECORDS_COUNT]
-        data[self.AVERAGE_PROFIT] = data[self.TOTAL_CAPITAL] / data[self.RECORDS_COUNT]
 
     async def _send_results_csv(self, results_for_csv: list[dict[str, any]]) -> None:
         if not results_for_csv:
@@ -89,11 +48,19 @@ class PerformaceSummaryCommand(TelegramCommand):
 
     def _get_columns(self) -> list[str]:
         return [
-            self.STRATEGY,
-            self.INSTANCE,
-            self.RECORDS_COUNT,
-            self.AVERAGE_CAPITAL,
-            self.AVERAGE_PROFIT,
-            self.TOTAL_CAPITAL,
-            self.TOTAL_PROFIT,
+            SummaryReportGenerator.STRATEGY,
+            SummaryReportGenerator.INSTANCE,
+            SummaryReportGenerator.DATE_FROM,
+            SummaryReportGenerator.DATE_TO,
+            SummaryReportGenerator.RECORDS_COUNT,
+            SummaryReportGenerator.PNL,
+            SummaryReportGenerator.AVG_PNL,
+            SummaryReportGenerator.BATTING_AVG,
+            SummaryReportGenerator.WIN_LOSS_RATIO,
+            SummaryReportGenerator.SHARPE_RATIO,
+            SummaryReportGenerator.AVG_ROI,
+            SummaryReportGenerator.MAX_LOSS,
+            SummaryReportGenerator.MAX_PROFIT,
+            SummaryReportGenerator.MAX_LOSS_PERCENT,
+            SummaryReportGenerator.MAX_PROFIT_PERCENT,
         ]
--- mirage/utils/date_utils.py
+++ mirage/utils/date_utils.py
@@ -6,3 +6,8 @@ def get_utc_datetime_for_filename() -> str:
     utc_now = datetime.datetime.now(datetime.timezone.utc)
     timestamp_millis = int(time.time() * 1000)
     return utc_now.strftime(f'%Y-%m-%dT%H-%M-%S_{timestamp_millis}')
+
+
+# Example expected format: 2024-01-16T16:47:12
+def iso_string_to_datetime(date_string: str) -> datetime.datetime:
+    return datetime.datetime.fromisoformat(date_string)
